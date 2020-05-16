f713afb08ed756eb735c2adc5cb7340aea8d5a43
Briws <54640696+brightwiths@users.noreply.github.com>
2020-05-16 19:09:17 +0530

    First

---
+++ release/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  autoplay: true,
+  items: 3,
+  loop: true,
+  navigation : true, 
+  slideSpeed : 300,
+  paginationSpeed : 400,
+  autoHeight:true
+  });
+});
